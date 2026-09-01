760f2f8786e877ba39936ec960bdef77ccdaeaa1
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-09-01 09:58:50 +0100

    updated auth controllers

--- controllers/authentication_controller.py
+++ controllers/authentication_controller.py
@@ -10,10 +10,12 @@ from dependencies import get_authentication_service
 router = APIRouter()
 
 @router.post("/register")
-def register_buyer(request:RegisterBuyerRequest,authentication_service:AuthenticationService=Depends(get_authentication_service)):
+def register_user(request:RegisterBuyerRequest,authentication_service:AuthenticationService=Depends(get_authentication_service)):
     return authentication_service.register_buyer(request)
 
 
 @router.post("/login")
-def login_buyer(request:LoginBuyerRequest,  authentication_service: AuthenticationService = Depends(get_authentication_service)):
+def login_user(request:LoginBuyerRequest,  authentication_service: AuthenticationService = Depends(get_authentication_service)):
     return  authentication_service.login_buyer(request)
+
+
