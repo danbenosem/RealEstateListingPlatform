4b7d0c66959a5d20797da580916926fd2035c281
Seunfunmi-ovie <oluwaseunpu@gmail.com>
2026-09-01 11:13:54 +0100

    Auth service,mapper,dtos updated

--- services/authenticaton_service.py
+++ services/authenticaton_service.py
@@ -1,5 +1,5 @@
-from Dtos.requests import RegisterUserRequest,LoginUserRequest
-from models.buyer import Buyer
+from Dtos.requests import RegisterUserRequest,LoginUserRequest,LogOutUserRequest
+from models.user import User
 from utils.mapper import UserMapper
 from Dtos.responses import RegisterResponse,LoginUserResponse,LogoutUserResponse
 from sqlalchemy.exc import IntegrityError
@@ -11,34 +11,27 @@ class AuthenticationService:
         self.repository = repository
 
     def register_user(self,request:RegisterUserRequest):
-        buyer= UserMapper.user_mapper(request)
+        user = UserMapper.user_mapper(request)
         try:
-            self.repository.save(buyer)
-            return  RegisterResponse(success=True, message="registration successful")
+            self.repository.save(user)
+            return RegisterResponse(success=True, message="registration successful")
         except IntegrityError:
-            return  RegisterResponse(success=False,message="user already exists")
+            return RegisterResponse(success=False, message="user already exists")
 
     def login_user(self, request:LoginUserRequest):
-        login_user=UserMapper.login_mapper(request)
-        buyer= self.repository.find_by_email(login_user.email)
-        if buyer is None:
+        login_user = UserMapper.login_mapper(request)
+        user = self.repository.find_by_email(login_user.email)
+        if user is None:
+            return LoginUserResponse(success=False, message="not registered")
 
-            return LoginUserResponse(success=False,message="not registered")
+        if user.password != login_user.password:
+            return LoginUserResponse(success=False, message="wrong password")
 
-        if buyer.password != login_user.password:
-            return LoginUserResponse(success=False,message="wrong password")
+        return LoginUserResponse(success=True, message="login successful", buyer_id=user.id)
 
-        return  LoginUserResponse(success=True,message="login successful",buyer_id=buyer.id)
+    def logout_user(self, request: LogOutUserRequest):
+        user = self.repository.find_by_email(request.email)
+        if user is None:
+            return LogoutUserResponse(success=False, message="user not found")
 
-
-    def logout_user(self):
-
-
-
-
-
-
-
-
-
-
+        return LogoutUserResponse(success=True, message="logout successful")
