5c01fbf1fd34d8c4758cb1b6c7349aceb06b578b
danbenosem <danielbenjami562@gmail.com>
2026-09-02 08:16:47 +0100

    made some corrections

--- controllers/buyer_controller.py
+++ controllers/buyer_controller.py
@@ -5,18 +5,18 @@ from dependencies import get_buyer_service
 
 router = APIRouter()
 
-@router.get("buyer/properties/price/{price}")
+@router.get("/buyer/properties/price/{price}")
 def find_property_by_price(
     price: int,buyer_service: BuyerService = Depends(get_buyer_service)):
     return buyer_service.find_property_by_price(price)
 
 
-@router.get("buyer/properties/location/{location}")
+@router.get("/buyer/properties/location/{location}")
 def find_property_by_location(location: str, buyer_service: BuyerService = Depends(get_buyer_service)):
     return buyer_service.find_property_by_location(location)
 
 
-@router.post("buyer/buy")
+@router.post("/buyer/buy")
 def buy_property(buyer_id:int,request:BuyPropertyRequest,buyer_service:BuyerService=Depends(get_buyer_service)):
     return buyer_service.buy_property(buyer_id,request)
 
